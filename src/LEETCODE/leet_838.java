d05f130d9767d983ed1b3a8353a82b87decce94a
jhy950210 <jhyy1995210@gmail.com>
2021-07-01 17:46:00 +0900

    리트코드 838번 풀이

--- src/LEETCODE/leet_838.java
+++ src/LEETCODE/leet_838.java
@@ -5,31 +5,56 @@ public class leet_838 {
 
     }
 
-    public static String pushDominoes(String dominoes) {
-        char[] dominoChar = dominoes.toCharArray();
-        String result = "";
-        boolean isForce = false;
+    public static String pushDominoes1(String dominoes) {
+        String tmp = dominoes;
 
-        for(int i=0; i<dominoChar.length; i++){
-            if(dominoChar[i] == 'L'){
-                result += "L";
-                isForce = false;
+        while(true){
+            String now = tmp;
+
+            if(now.indexOf("R.L") != -1){
+                now = now.replace("R.L","S");
             }
-            if(dominoChar[i] == 'R'){
-                result += "R";
-                isForce = true;
+            if(now.indexOf(".L") != -1){
+                now = now.replace(".L","LL");
             }
-            if(dominoChar[i] == '.'){
-                if(isForce && dominoChar[i+1] == 'L'){
-                    result += ".";
-                } else if(isForce){
-                    result += "R";
-                } else if(!isForce && dominoChar[i+1] == 'L'){
-                    result += "L";
-                }
+            if(tmp.indexOf("R.") != -1){
+                now = now.replace("R.","RR");
             }
+            if(now.equals(tmp)) break;
+
+            tmp = now;
         }
 
-        return "";
+
+        return tmp.replace("S","R.L");
+    }
+
+    public static String pushDominoes2(String S) {
+        char[] A = S.toCharArray();
+        int N = A.length;
+        int[] forces = new int[N];
+
+        // Populate forces going from left to right
+        int force = 0;
+        for (int i = 0; i < N; ++i) {
+            if (A[i] == 'R') force = N;
+            else if (A[i] == 'L') force = 0;
+            else force = Math.max(force - 1, 0);
+            forces[i] += force;
+        }
+
+        // Populate forces going from right to left
+        force = 0;
+        for (int i = N-1; i >= 0; --i) {
+            if (A[i] == 'L') force = N;
+            else if (A[i] == 'R') force = 0;
+            else force = Math.max(force - 1, 0);
+            forces[i] -= force;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int f: forces)
+            ans.append(f > 0 ? 'R' : f < 0 ? 'L' : '.');
+        return ans.toString();
     }
 }
