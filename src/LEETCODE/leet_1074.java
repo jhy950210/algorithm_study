9fd311c87faaf9f733cdb359ff009500412d1107
jhy950210 <jhyy1995210@gmail.com>
2021-07-09 17:01:23 +0900

    1074번 풀이 추가

--- src/LEETCODE/leet_1074.java
+++ src/LEETCODE/leet_1074.java
@@ -1,42 +1,58 @@
 package src.LEETCODE;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class leet_1074 {
     public static void main(String[] args) {
 
     }
 
+    static int dx[] = {0,0,1,-1};
+    static int dy[] = {1,-1,0,0};
+
+
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
         Queue<Integer> que = new LinkedList<>();
         int count = 0;
-        int sum = 0;
 
         int rowSize = matrix.length;
         int colSize = matrix[0].length;
-        //int[] cache = new int[size+1];
+        HashSet<List> cache = new HashSet<>();
 
-        que.add(matrix[0][0]);
+        for(int i=0; i<rowSize; i++){
+            for(int j=0; j<colSize; j++){
+                int sum = 0;
+                ArrayList<int[][]> matrixList = new ArrayList();
 
-        while(!que.isEmpty()){
-            int queSize = que.size();
-            int poll = que.poll();
+                que.add(matrix[i][j]);
+                matrixList.add(new int[i][j]);
 
-            for(int i=0; i<queSize; i++){
-                sum += poll;
+                while(!que.isEmpty()){
+                    //int queSize = que.size();
+                    int poll = que.poll();
 
-                if(sum == target){
-                    count++;
-                }
+                    sum += poll;
 
-                if(int r < rowSize && int c < colSize){
+                    if(sum == target && !cache.contains(matrixList)){
+                        cache.add(matrixList);
+                        count++;
+                    }
 
+                    for(int k=0; k<4; j++){
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+
+                        if(nx>=0 && ny>=0 && nx<rowSize && ny<colSize) {
+                            matrixList.add(new int[nx][ny]);
+                            que.add(matrix[nx][ny]);
+                        }
+
+                    }
                 }
             }
         }
 
-        return 0;
+        return count;
     }
+
 }
